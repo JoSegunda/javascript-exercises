9739f04963906b9801fb4bf712302318e335950a
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-25 08:33:15 +0000

    updated exe001

--- JavaScript_fundamental/exe001.js
+++ JavaScript_fundamental/exe001.js
@@ -1,5 +1,5 @@
-const csv_to_array = (data, delimiter=',', omitFirstRow=true) =>
-    data.slice(omitFirstRow ? data.indexOf('\n')+1:0)
+const csv_to_array = (data, delimiter=',', omitFirstRow=false) =>
+    data.slice(omitFirstRow ? data.indexOf('\n')+1:0).split('\n').map(v => v.split(delimiter))
     
 
 console.log(csv_to_array('a,b\nc,d'))
