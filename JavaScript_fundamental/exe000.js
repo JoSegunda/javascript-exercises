a5c6533dbb7293a6b49a4db4c6d51b0726cf18e6
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-25 01:21:41 +0000

    added exe000

--- JavaScript_fundamental/exe000.js
+++ JavaScript_fundamental/exe000.js
@@ -1,6 +1,6 @@
 
 const matches = (obj, source) =>
-    Object.keys(source).every(key => obj.hasOwnProperty(key) && obj[key] == source[key])
+    Object.keys(source).every(key => obj.hasOwnProperty(key) && obj[key] === source[key])
 
 // Test cases:
 console.log(matches({ age: 25, hair: 'long', beard: true }, { hair: 'long', beard: true })); // true
